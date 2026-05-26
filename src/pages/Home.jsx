bb90e88e1e9db1aed8b0c0cb65167579de8b1a2f
zensuki <115860137+zensukix@users.noreply.github.com>
2026-05-26 11:44:29 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,6 +6,7 @@ import Card from "../components/Card";
 const Home = (props) => {
   return (
     <div>
+        <Navbar/>
       <HeroSection />
       <div className=" w-full flex flex-wrap justify-center items-center gap-4">
         <Card />
